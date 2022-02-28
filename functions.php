d3b1a0c18884fa3dde16335135c0b44c2f09af71
Tom Schlick <tom.schlick@quartzy.com>
2022-02-28 11:18:46 -0500

    commenting out function that is conflicting with Laravel native env()
    /cc @sonnysavage

--- functions.php
+++ functions.php
@@ -186,72 +186,72 @@ if (!function_exists('pj')) {
 
 }
 
-if (!function_exists('env')) {
-    /**
-     * Gets an environment variable from available sources, and provides emulation
-     * for unsupported or inconsistent environment variables (i.e. DOCUMENT_ROOT on
-     * IIS, or SCRIPT_NAME in CGI mode). Also exposes some additional custom
-     * environment information.
-     *
-     * @param string $key Environment variable name.
-     * @param string|null $default Specify a default value in case the environment variable is not defined.
-     * @return string|bool|null Environment variable setting.
-     * @link https://book.cakephp.org/3.0/en/core-libraries/global-constants-and-functions.html#env
-     */
-    function env($key, $default = null)
-    {
-        if ($key === 'HTTPS') {
-            if (isset($_SERVER['HTTPS'])) {
-                return (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off');
-            }
+// if (!function_exists('env')) {
+//     /**
+//      * Gets an environment variable from available sources, and provides emulation
+//      * for unsupported or inconsistent environment variables (i.e. DOCUMENT_ROOT on
+//      * IIS, or SCRIPT_NAME in CGI mode). Also exposes some additional custom
+//      * environment information.
+//      *
+//      * @param string $key Environment variable name.
+//      * @param string|null $default Specify a default value in case the environment variable is not defined.
+//      * @return string|bool|null Environment variable setting.
+//      * @link https://book.cakephp.org/3.0/en/core-libraries/global-constants-and-functions.html#env
+//      */
+//     function env($key, $default = null)
+//     {
+//         if ($key === 'HTTPS') {
+//             if (isset($_SERVER['HTTPS'])) {
+//                 return (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off');
+//             }
 
-            return (strpos((string)env('SCRIPT_URI'), 'https://') === 0);
-        }
+//             return (strpos((string)env('SCRIPT_URI'), 'https://') === 0);
+//         }
 
-        if ($key === 'SCRIPT_NAME' && env('CGI_MODE') && isset($_ENV['SCRIPT_URL'])) {
-            $key = 'SCRIPT_URL';
-        }
+//         if ($key === 'SCRIPT_NAME' && env('CGI_MODE') && isset($_ENV['SCRIPT_URL'])) {
+//             $key = 'SCRIPT_URL';
+//         }
 
-        $val = null;
-        if (isset($_SERVER[$key])) {
-            $val = $_SERVER[$key];
-        } elseif (isset($_ENV[$key])) {
-            $val = $_ENV[$key];
-        } elseif (getenv($key) !== false) {
-            $val = getenv($key);
-        }
+//         $val = null;
+//         if (isset($_SERVER[$key])) {
+//             $val = $_SERVER[$key];
+//         } elseif (isset($_ENV[$key])) {
+//             $val = $_ENV[$key];
+//         } elseif (getenv($key) !== false) {
+//             $val = getenv($key);
+//         }
 
-        if ($key === 'REMOTE_ADDR' && $val === env('SERVER_ADDR')) {
-            $addr = env('HTTP_PC_REMOTE_ADDR');
-            if ($addr !== null) {
-                $val = $addr;
-            }
-        }
+//         if ($key === 'REMOTE_ADDR' && $val === env('SERVER_ADDR')) {
+//             $addr = env('HTTP_PC_REMOTE_ADDR');
+//             if ($addr !== null) {
+//                 $val = $addr;
+//             }
+//         }
 
-        if ($val !== null) {
-            return $val;
-        }
+//         if ($val !== null) {
+//             return $val;
+//         }
 
-        switch ($key) {
-            case 'DOCUMENT_ROOT':
-                $name = env('SCRIPT_NAME');
-                $filename = env('SCRIPT_FILENAME');
-                $offset = 0;
-                if (!strpos($name, '.php')) {
-                    $offset = 4;
-                }
+//         switch ($key) {
+//             case 'DOCUMENT_ROOT':
+//                 $name = env('SCRIPT_NAME');
+//                 $filename = env('SCRIPT_FILENAME');
+//                 $offset = 0;
+//                 if (!strpos($name, '.php')) {
+//                     $offset = 4;
+//                 }
 
-                return substr($filename, 0, -(strlen($name) + $offset));
-            case 'PHP_SELF':
-                return str_replace(env('DOCUMENT_ROOT'), '', env('SCRIPT_FILENAME'));
-            case 'CGI_MODE':
-                return (PHP_SAPI === 'cgi');
-        }
+//                 return substr($filename, 0, -(strlen($name) + $offset));
+//             case 'PHP_SELF':
+//                 return str_replace(env('DOCUMENT_ROOT'), '', env('SCRIPT_FILENAME'));
+//             case 'CGI_MODE':
+//                 return (PHP_SAPI === 'cgi');
+//         }
 
-        return $default;
-    }
+//         return $default;
+//     }
 
-}
+// }
 
 if (!function_exists('triggerWarning')) {
     /**
